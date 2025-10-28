92cf11e65bcf1952743c8512310251bf054e638d
Djalal <djalal.aymn@gmail.com>
2025-10-28 05:00:35 +0100

    last one

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,11 @@
 module.exports = function (api) {
+   
+ 
   api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-    ],
-    plugins: [
-      // Worklets plugin required by expo-router / reanimated worklets
-      'react-native-worklets/plugin',
     ],
   };
 };
