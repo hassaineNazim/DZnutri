34a7850d9d95da149909b0402c39e1a994781e54
hassaineNazim <hassaine.nazim2@gmail.com>
2025-11-24 17:57:27 +0100

    changement mineur

--- app/config/api.js
+++ app/config/api.js
@@ -22,7 +22,7 @@ const getApiUrl = () => {
    
 
     // Fallback: previously configured local IP (update if your machine IP differs)
-    return 'http://192.168.1.71:8000';
+    return 'http://10.236.142.150:8000';
 };
 
 export const API_URL = getApiUrl();
